8d011b40e38a656b0284eda4a83e89cf33ecc988
Minh Le <lenguyetminh1998@gmail.com>
2017-12-20 16:47:15 -0500

    Camera working

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -30,38 +30,38 @@ export class DashboardPage {
   async takePicture(): Promise<any>{
     try{
       this.image = await this.camera.getPicture(this.options);
-    }
+      }
     catch(e){
-      console.log(e);
+      console.error(e);
     }
 
   }
 
 
 
-
-  ionViewWillLoad() {
-
-    //Subscribe to Firebase Authentication state, similar to onAuthStateChanged
-    this.afAuth.authState.subscribe(data => {
-
-      // console.log("data", data);
-      if (data && data.email && data.uid){
-        this.toast.create({
-          message: `Welcome to Snapbit, ${data.email}`,
-          duration: 3500
-        }).present();
-      }
-      else
-      {
-        this.toast.create({
-          message: `Could not find authentication detail!`,
-          duration: 3500
-        }).present();
-      }
-
-    });
-  }
+  //
+  // ionViewWillLoad() {
+  //
+  //   //Subscribe to Firebase Authentication state, similar to onAuthStateChanged
+  //   this.afAuth.authState.subscribe(data => {
+  //
+  //     // console.log("data", data);
+  //     if (data && data.email && data.uid){
+  //       this.toast.create({
+  //         message: `Welcome to Snapbit, ${data.email}`,
+  //         duration: 3500
+  //       }).present();
+  //     }
+  //     else
+  //     {
+  //       this.toast.create({
+  //         message: `Could not find authentication detail!`,
+  //         duration: 3500
+  //       }).present();
+  //     }
+  //
+  //   });
+  // }
 
 
 }
