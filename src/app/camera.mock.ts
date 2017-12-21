0060634b592e5e37d53b89a5affc913d603c5be7
Minh Le <lenguyetminh1998@gmail.com>
2017-12-21 15:57:32 -0500

    Image storage done

--- src/app/camera.mock.ts
+++
@@ -1,9 +0,0 @@
-// import { Camera } from '@ionic-native/camera';
-//
-// export class CameraMock extends Camera{
-//   getPicture(options){
-//     return new Promise((resolve, reject) => {
-//
-//     })
-//   }
-// }
--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+import * as firebase from 'firebase';
 
 @IonicPage()
 @Component({
@@ -11,7 +14,7 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 export class DashboardPage {
 
   //------------------------- LIST OF COMPONENTS ---------------------------
-  image: string;
+  currentUserUid: any;
   options: CameraOptions = {
     quality: 100,
     destinationType: this.camera.DestinationType.DATA_URL,
@@ -26,42 +29,49 @@ export class DashboardPage {
   }
 
 
-  //---------------------- FUNCTION THAT WILL RETURN A PROMISE ------------
-  async takePicture(): Promise<any>{
-    try{
-      this.image = await this.camera.getPicture(this.options);
-      }
-    catch(e){
-      console.error(e);
-    }
 
+
+  //------------- TAKE A PICTURE --------------------
+  takePicture(){
+    this.camera.getPicture(this.options).then((imageData) => {
+
+      //Send picture to firebase storage
+       const imageObjectRef = firebase.storage().ref('profilePictures/' + this.currentUserUid + '/profilePicture.jpg');
+       imageObjectRef.putString(imageData, 'base64', {contentType:'image/jpg'});
+       console.log("Successfully pushed to database");
+
+
+    }, (err) => {
+      console.log(err);
+    });
   }
 
 
 
-  //
-  // ionViewWillLoad() {
-  //
-  //   //Subscribe to Firebase Authentication state, similar to onAuthStateChanged
-  //   this.afAuth.authState.subscribe(data => {
-  //
-  //     // console.log("data", data);
-  //     if (data && data.email && data.uid){
-  //       this.toast.create({
-  //         message: `Welcome to Snapbit, ${data.email}`,
-  //         duration: 3500
-  //       }).present();
-  //     }
-  //     else
-  //     {
-  //       this.toast.create({
-  //         message: `Could not find authentication detail!`,
-  //         duration: 3500
-  //       }).present();
-  //     }
-  //
-  //   });
-  // }
+
+  ionViewWillLoad() {
+
+    //Subscribe to Firebase Authentication state, similar to onAuthStateChanged
+    this.afAuth.authState.subscribe(data => {
+
+      // console.log("data", data);
+      if (data && data.email && data.uid){
+        this.currentUserUid = data.uid;
+        this.toast.create({
+          message: `Welcome to Snapbit, ${data.email}`,
+          duration: 3500
+        }).present();
+      }
+      else
+      {
+        this.toast.create({
+          message: `Could not find authentication detail!`,
+          duration: 3500
+        }).present();
+      }
+
+    });
+  }
 
 
 }
